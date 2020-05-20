498b733638b4c57c3ca9166437403ecbba36a2c0
Rafael Benegas <rafael.benegas@konecta.com.py>
2020-05-20 18:09:23 -0400

    Drop macaddr attribute

--- src/db.rs
+++ src/db.rs
@@ -4,9 +4,9 @@ use tokio_pg_mapper::FromTokioPostgresRow;
 use std::io;
 
 pub async fn create_redirect(client: &Client, red: &Redirects) -> () {
-    let statement = client.prepare("insert into redirects (caller_ip, caller_macaddr, called_url) values ($1, $2, $3)").await.unwrap();
+    let statement = client.prepare("insert into redirects (caller_ip, called_url) values ($1, $2)").await.unwrap();
 
-    client.query(&statement, &[ &red.caller_ip, &red.caller_macaddr, &red.called_url])
+    client.query(&statement, &[ &red.caller_ip, &red.called_url])
         .await
         .expect("Error creating an entry of redirect");
 }
